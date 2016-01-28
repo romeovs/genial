68cb91448ca1c2484bc07b8bb60ce7746cff68d9
Romeo Van Snick <romeovs@gmail.com>
2016-01-28 14:13:22 +0100

    add example dir

---
+++ examples/genfile.js
@@ -0,0 +1,36 @@
+import generator from './src/lib/generator'
+const {
+  types
+, actions } = generator;
+
+const example =
+  generator('example')
+    .parameter('name', {
+      type: types.string.require
+    , description: 'your user name'
+    , question: 'what is your name?'
+    // , default: 'nice'
+    , validate (x) {
+        if (x === 'milo') {
+          throw new Error('\'milo\' is not allowed');
+        }
+      }
+    })
+    .parameter('email', {
+      description: 'email address'
+    , default: 'romeov'
+    , type: types.email.require
+    , positional: true
+    })
+    .parameter('foo', {
+      type: types.bool
+    , aliases: ['f']
+    })
+    .action(actions.add, {
+      from: './templates/foo.txt'
+    , to:   './components/{{name}}.txt'
+    })
+
+export default [
+  example
+];
